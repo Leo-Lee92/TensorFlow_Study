2276819a26bf443104031bfe41a5d5dd3fae1b85
Leo-LEE92 <messy92@unist.ac.kr>
2020-05-31 11:41:48 +0900

    오타 수정

--- Ch.12.3.6_사용자정의모델.py
+++ Ch.12.3.6_사용자정의모델.py
@@ -7,4 +7,16 @@ import copy
 import random
 
 # %%
-class Residual
+class ResidualBlock(keras.layers.Layer):
+    def __init__(self, n_layers,  n_neurons, **kwargs):
+        super().__init__(**kwargs)
+        self.hidden = [keras.layers.Dense(n_neurons, activation = "elu", kernel_initalizer = "he_normal") for _ in range(n_layers)]
+
+    def call(self, inputs):
+        Z = inputs
+        for layer in self.hidden:
+            Z = layer(Z)
+        return inputs + Z
+
+
+# %%
